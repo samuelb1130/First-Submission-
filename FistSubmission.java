da8fdb978e14d79311468b0b8c1edbfd944e2f0a
sam bauer <42975601+samuelb1130@users.noreply.github.com>
2018-09-17 18:14:19 -0400

    Add files via upload

---
+++ FistSubmission.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Sam
+ *
+ *
+ */
+public class FistSubmission 
+{
+	public static void main (String[]args)
+	{
+		System.out.print("hello world, I have submitted my first project using github"); 
+	}
+}
